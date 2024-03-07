f4ea35613e7074a30ee4aadf2da188110f3cd2f3
Mihamina <mihamina.rakotomandimby@rktmb.org>
2024-03-07 20:01:54 +0300

    make it a bit clean

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 // @ts-check
 // This is an eslint configuration file for TypeScript projects.
-// Add the nedded configuration in order to allow the usage of "any" type.
+// Add the nedded configuration in order to exclude "./docs" and "./dist" folders from linting.
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
@@ -10,6 +10,8 @@ export default tseslint.config(
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
+      // disable no-async-promise-executor
+      'no-async-promise-executor': 'off',
     },
   },
 );
